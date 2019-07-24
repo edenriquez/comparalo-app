9e364ff740fb4f51ef710a8af56312459e741554
Eduardo Enriquez <fsociety@MacBook-Air-de-Eduardo.local>
2019-07-24 01:26:03 -0500

    First endpoints definition

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+import {
+  db
+} from '../database/db';
+import {
+  log
+} from 'util';
 
-router.post('/search', function (req, res) {
-  console.log('HERE', req)
-  res.send('!');
+router.get('/categories', function (req, res) {
+  res.json(db);
+});
+
+router.get('/categories/:category', function (req, res) {
+  const response = db.categories.find(i => req.params.category == i.category)
+  res.json(response);
+});
+
+router.get('/categories/:category/products', function (req, res) {
+  const response = db.categories.find(i => req.params.category == i.category)
+  res.json(response.products);
+});
+
+router.put('/categories/:category/products/:id', function (req, res) {
+  log(req.body)
+  const response = db.categories.find(i => req.params.category == i.category)
+  const product = response.products.filter((i) => {
+    if (req.params.id == i.id) {
+      i.status = req.params.status
+    }
+  })
+  res.json(product);
 });
 
 module.exports = router;
