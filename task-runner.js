7e020bea04f64ada9aea09131e962aac5324064d
Eduardo Enriquez <fsociety@MacBook-Air-de-Eduardo.local>
2019-09-25 01:47:28 -0500

    Ability to run local scripts to emulate puppeteer browser

--- task-runner.js
+++ task-runner.js
@@ -1,5 +1,9 @@
 let args = process.argv;
 const filePath = args.slice(2)[0]
 const funcName = args.slice(2)[1]
-
-eval(`require(\"${filePath}\")\.${funcName}()`)
+const existParams = args.slice(2).slice(2).join(',')
+if (existParams) {
+  eval(`require(\"${filePath}\")\.${funcName}(\"${existParams}\")`)
+} else {
+  eval(`require(\"${filePath}\")\.${funcName}()`)
+}
