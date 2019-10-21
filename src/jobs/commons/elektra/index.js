7f1b28cc8b6d74dd0b436c181db84aa048f4803e
Eduardo Enriquez <fsociety@MacBook-Air-de-Eduardo.local>
2019-10-20 21:53:06 -0500

    Fix elektra class to scrap

--- src/jobs/commons/elektra/index.js
+++ src/jobs/commons/elektra/index.js
@@ -1,15 +1,9 @@
 module.exports.getElektraPrice = async (page) => {
-  const prices = (await page.$x("//*[starts-with(@id,'buybox_price')]/span"))[0]
-  const textPrice = (await page.evaluate(el => {
-    return el.textContent;
-  }, prices));
-  const response = parseFloat(textPrice.replace('$', '').replace(',', ''))
+  const priceByClass = await page.$eval('.skuBestPrice', el => el.textContent);
+  const response = parseFloat(priceByClass.replace('$', '').replace(',', ''))
   return response ? response : 0
 }
 module.exports.getElektraName = async (page) => {
-  const articlePath = (await page.$x('//*[starts-with(@id,"productName_nameProduct")]'))[0];
-  const nameByPath = await page.evaluate(el => {
-    return el.textContent;
-  }, articlePath);
-  return nameByPath.trim();
+  const nameByClass = await page.$eval('.productName', el => el.textContent);
+  return nameByClass.trim();
 }
