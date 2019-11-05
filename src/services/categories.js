ee3da0e595e2989115ea05ed4003f4855160a922
Eduardo Enriquez <fsociety@MacBook-Air-de-Eduardo.local>
2019-11-04 23:55:24 -0600

    :dizzy: Setting up category endpoints

--- src/services/categories.js
+++ src/services/categories.js
@@ -9,3 +9,32 @@ module.exports.getAllCategories = () => {
     resolve(result)
   })
 }
+module.exports.filterCategory = (categoryName) => {
+  return new Promise(async (resolve, reject) => {
+    const result = await models.Category.findCategory(categoryName);
+    if (!result) {
+      reject(result)
+    }
+    resolve(result)
+  })
+}
+
+module.exports.filterProducts = (categoryName) => {
+  return new Promise(async (resolve, reject) => {
+    const result = await models.Category.filterProducts(categoryName);
+    if (!result) {
+      reject(result)
+    }
+    resolve(result)
+  })
+}
+
+module.exports.getProductById = (id) => {
+  return new Promise(async (resolve, reject) => {
+    const result = await models.Category.getProductById(id);
+    if (!result) {
+      reject(result)
+    }
+    resolve(result)
+  })
+}
