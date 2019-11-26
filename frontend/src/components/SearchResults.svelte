4490cb8d98feb96cf9d2361c4c3edfe6e6cf4d35
Eduardo Enriquez <fsociety@MacBook-Air-de-Eduardo.local>
2019-11-26 01:03:43 -0600

    :bowtie: Start doing search result bar

--- frontend/src/components/SearchResults.svelte
+++ frontend/src/components/SearchResults.svelte
@@ -20,9 +20,15 @@
 <div
   id="search-results"
   class="search-results-container flex flex-wrap w-full my-4 mx-20">
-  <div class="search-results">
+  <div class="search-results w-full">
+    <div class=" p-4 mx-2 my-2">
+      <strong>Results</strong>
+    </div>
+    <hr />
     {#each results as result}
-      <div class="result p-4 mx-2 my-2">{result.product_name}</div>
+      <div class="result p-4 mx-1 my-1">
+        <span>{result.product_name.substring(0, 10)}</span>
+      </div>
     {/each}
   </div>
 </div>
