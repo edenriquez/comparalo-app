a6fe56a65d0d91c89c3003d5e6c6fe61fc785f1f
Eduardo Enriquez <fsociety@MacBook-Air-de-Eduardo.local>
2019-11-26 22:53:52 -0600

    :straight_ruler: Update styles for search bar

--- frontend/src/components/SearchResults.svelte
+++ frontend/src/components/SearchResults.svelte
@@ -4,10 +4,12 @@
 
 <style>
   .search-results-container {
+    margin-top: -50px;
     background: rgb(247, 241, 241);
     border-radius: 15px;
   }
   .search-results {
+    margin-top: 50px;
     max-height: 250px;
     overflow-y: scroll;
   }
@@ -19,12 +21,8 @@
 
 <div
   id="search-results"
-  class="search-results-container flex flex-wrap w-full my-4 mx-20">
+  class="search-results-container flex flex-wrap w-full mx-20">
   <div class="search-results w-full">
-    <div class=" p-4 mx-2 my-2">
-      <strong>Results</strong>
-    </div>
-    <hr />
     {#each results as result}
       <div class="result p-4 mx-1 my-1">
         <span>{result.product_name.substring(0, 10)}</span>
