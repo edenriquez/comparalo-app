e0d9df8a7bb3ce31b02c1bc78845ec58301605a2
Eduardo Enriquez <eduardo.enriquez@wizeline.com>
2019-11-27 23:57:48 -0600

    :star: Adding skeleton loader content for search bar

--- frontend/src/components/SearchResults.svelte
+++ frontend/src/components/SearchResults.svelte
@@ -1,8 +1,46 @@
 <script>
   export let results;
+  export let resultShouldRender;
 </script>
 
 <style>
+.skeleton-box {
+  position: relative;
+  overflow: hidden;
+  background-color: #e2e8f0;
+}
+
+  .skeleton-box::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    -webkit-transform: translateX(-100%);
+            transform: translateX(-100%);
+    background-image: linear-gradient(
+      90deg,
+      rgba(255, 255, 255, 0) 0,
+      rgba(255, 255, 255, 0.2) 20%,
+      rgba(255, 255, 255, 0.5) 60%,
+      rgba(255, 255, 255, 0)
+    );
+    -webkit-animation: shimmer 3s infinite;
+            animation: shimmer 3s infinite;
+    content: '';
+  }
+@-webkit-keyframes shimmer {
+  100% {
+    -webkit-transform: translateX(100%);
+            transform: translateX(100%);
+  }
+}
+@keyframes shimmer {
+  100% {
+    -webkit-transform: translateX(100%);
+            transform: translateX(100%);
+  }
+}
   .search-results-container {
     margin-top: -50px;
     background: rgb(247, 241, 241);
@@ -18,15 +56,38 @@
     border-radius: 15px;
   }
 </style>
-
-<div
-  id="search-results"
-  class="search-results-container flex flex-wrap w-full mx-20">
-  <div class="search-results w-full">
-    {#each results as result}
-      <div class="result p-4 mx-1 my-1">
-        <span>{result.product_name.substring(0, 10)}</span>
+{#if resultShouldRender}
+  <div
+    id="search-results"
+    class="search-results-container flex flex-wrap w-full mx-20">
+    <div class="search-results w-full">
+    {#if results.length > 0}
+      {#each results as result}
+        <div class="result p-4 mx-1 my-1">
+          <span>{result.product_name.substring(0, 10)}</span>
+        </div>
+      {/each}
+    {:else}
+      <div class="bg-gray-100 w-full p-8 flex flex-wrap">
+        <div class="w-full md:w-1/3 lg:w-1/5 p-2">
+          <a href="/detail/72112620-skoda-octavia-combi-iii-combi-style-green-tec" class="flex flex-col relative w-full bg-white overflow-hidden card translate-3d-none-after relative w-full bg-white overflow-hidden card translate-3d-none-after rounded border border-gray-300">
+          <div class="relative group text-primary-500" style="padding-top: 70%">
+            <div class="absolute top-0 left-0 h-full w-full"><span class="skeleton-box group-hover:scale-110 transition-transform transform-center block h-full"></span></div></div>
+          <div class="flex flex-col flex-grow">
+            <div class="pl-4 pr-4 pt-4 mb-4 text-left relative flex-grow">
+              <h3
+                  class="text-lg font-bold text-gray-darkest mr-10"><span class="skeleton-box h-5 w-1/6 inline-block"></span>
+                <span class="skeleton-box h-5 w-1/2 inline-block"></span>
+                <span class="skeleton-box h-5 w-2/4 inline-block"></span>
+                <span class="skeleton-box h-5 w-2/5 inline-block"></span>
+              </h3>
+            </div>        
+          </div>
+          </a>
+        </div>
       </div>
-    {/each}
+    {/if}
+    </div>
   </div>
-</div>
+
+{/if}
