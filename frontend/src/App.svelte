a379969183042459c0f0a4af6a362443eba7b7cf
Eduardo Enriquez <fsociety@MacBook-Air-de-Eduardo.local>
2019-12-05 00:46:14 -0600

    :straight_ruler: Adding router fixes and charts

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,20 @@
 <script>
-  import { setContext } from 'svelte'
-  import Router from 'svelte-spa-router';
-  import {routes} from './routes.js'
+  import { setContext } from "svelte";
+  import Router from "svelte-spa-router";
+  import { routes } from "./routes.js";
 
   export let headerProps;
-  setContext('HeaderProps', headerProps)
-  
+  setContext("HeaderProps", headerProps);
 </script>
+
 <style>
+  @import url("https://fonts.googleapis.com/css?family=Oswald&display=swap");
   :global(body) {
-      margin: 0;
-      padding: 0;
-    }
+    margin: 0;
+    padding: 0;
+  }
 </style>
+
 <body>
-    <Router {routes}/>
+  <Router {routes} />
 </body>
